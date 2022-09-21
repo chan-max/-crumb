6ffb408c209de9b200b002bd49a6545e1a0a35f9
2651308363@qq.com <2651308363@qq.com>
2022-09-21 13:57:55 +0800

    init

--- src/components/crButton.ts
+++ src/components/crButton.ts
@@ -1,6 +1,61 @@
+import { onMounted, useProps, useParent } from 'crushed'
+
+export const crButtonGroup = {
+    template: /*html*/`
+        <div .cr-button-group>
+            <style>
+                button{
+                    color!:red;
+                }
+            </style>
+            <slot>
+        </div>
+    `,
+    create() {
+        onMounted(() => {
+
+        })
+    }
+}
 
 export const crButton = {
-    template:/*html*/`
-        
-    `
+    props: {
+        color: {
+            default: '#00c1de',
+        },
+        type: {
+            default: ''
+        },
+        disabled: {
+            default: false
+        }
+    },
+    template: /*html*/`
+        <style scoped>
+            .cr-button{
+                transition:all 0.3s;
+                color:#fff;
+                padding: 7px 15px;
+                border:none;
+                $box-shadow: ['5px','5px','20px',opacity(color,0.25)];
+                $cursor: disabled ?  'not-allowed' : 'pointer';
+                $background-color:color;
+                border-radius:3px;
+                &:hover{
+                    $background-color:darken(color,7);
+                }
+                &:focus{
+                    outline:0;
+                    $box-shadow:[0, 0, 0 ,'0.25rem', opacity(color,0.25)];
+                }
+            }
+        </style>
+        <button .cr-button>
+            <slot>
+        </button>
+    `,
+    create({
+        $instance
+    }: any) {
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { createApp } from 'crushed'
-
+import { crButton ,crButtonGroup} from './components/crButton'
+import 'font-awesome/css/font-awesome.min.css'
 let root = {
     template:/*html*/ `
-    
-    `
+        <cr-button-group>
+            <cr-button > click </cr-button>
+            <cr-button> click </cr-button>
+            <cr-button> click </cr-button>
+        </cr-button-group>
+    `,
+    create({ $self }: any) {
+        $self.i = 0
+        $self.add = () => $self.i++
+    }
 }
 
 
+
 let app = createApp(root)
+
+app.component('crButton', crButton)
+app.component('crButtonGroup', crButtonGroup)
+
 
 app.mount()
